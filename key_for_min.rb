5c0925c7be7f9ab7a206fb91b32da7e24ee0b7da
Grayson Cowing <grayson.cowing@gmail.com>
2018-02-17 21:28:09 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,7 +2,7 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-  if name_hash == []
+  if name_hash.size == 0
     nil
   else
     name_hash.collect {|key, value| return key if name_hash.all? {|name,val| value <= val}}
